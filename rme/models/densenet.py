19a15cab7d7434b5cf0478e0073454bc98e3ea79
Roberto Estevão <robertomest@poli.ufrj.br>
2017-03-21 16:29:31 -0300

    Added implementation for Densenet-BC

--- rme/models/densenet.py
+++ rme/models/densenet.py
@@ -7,56 +7,92 @@ from keras.regularizers import l2
 from rme.datasets import cifar10, cifar100, svhn, mnist, preprocessing
 from rme.callbacks import Step
 
-def preact_layer(x, num_channels, l2_reg, dropout, kernel_size=3):
+def preact_layer(x, num_channels, l2_reg, dropout, kernel_size=3,
+                 name='layer'):
     '''
     Adds a preactivation layer for the densenet. This also includes l2
     reagularization on BatchNorm learnable parameters as in the original
     implementation.
     '''
+
     out = BatchNormalization(gamma_regularizer=l2(l2_reg),
-                             beta_regularizer=l2(l2_reg))(x)
-    out = Activation('relu')(out)
+                             beta_regularizer=l2(l2_reg),
+                             name=name + '_bn')(x)
+    out = Activation('relu', name=name + '_relu')(out)
     out = Convolution2D(num_channels, kernel_size, kernel_size,
                         border_mode='same', init='he_normal',
-                        W_regularizer=l2(l2_reg), bias=False)(out)
+                        W_regularizer=l2(l2_reg), bias=False,
+                        name=name + '_conv')(out)
     if dropout > 0:
         out = Dropout(dropout)(out)
     return out
 
-def dense_block(x, num_layers, growth_rate, l2_reg, dropout):
+def bottleneck_layer(x, num_channels, l2_reg, dropout, kernel_size=3,
+                     name='layer'):
+    '''
+    DenseNet-B: 1x1 conv bottleneck before 3x3 conv
+    '''
+    o = preact_layer(x, num_channels*4, l2_reg, dropout, kernel_size=1,
+                     name=name+'_bottleneck')
+    o = preact_layer(o, num_channels, l2_reg, dropout, kernel_size=kernel_size,
+                     name=name+'_main')
+
+    return o
+
+def dense_block(x, num_layers, growth_rate, l2_reg, dropout, bottleneck,
+                name='block'):
     '''
     Adds a dense block for the densenet.
     '''
     for i in range(num_layers):
         # Get layer output
-        out = preact_layer(x, growth_rate, l2_reg, dropout)
+        if bottleneck:
+            out = bottleneck_layer(x, growth_rate, l2_reg, dropout,
+                                   name=name+'_layer%d' %(i + 1))
+        else:
+            out = preact_layer(x, growth_rate, l2_reg, dropout,
+                               name=name+'_layer%d' %(i + 1))
         # Merge them on the channel axis
         merge_axis = -1
         # Concatenate input with layer ouput
-        x = merge([x, out], mode='concat', concat_axis=merge_axis)
+        x = merge([x, out], mode='concat', concat_axis=merge_axis,
+                  name=name + '_layer%d_concat' %(i + 1))
     return x
 
-def transition_block(x, num_channels, l2_reg, dropout):
+def transition_block(x, num_channels, l2_reg, dropout, name='transition'):
     '''
     Adds a transition block for the densenet. This halves the spatial
     dimensions.
     '''
-    x = preact_layer(x, num_channels, l2_reg, dropout, kernel_size=1)
+    x = preact_layer(x, num_channels, l2_reg, dropout, kernel_size=1,
+                     name=name + '_conv')
     # x = Convolution2D(n_channels, 1, 1, border_mode='same',
     #                   init='he_normal', W_regularizer=l2(l2_reg))(x)
-    x = AveragePooling2D()(x)
+    x = AveragePooling2D(name=name + '_pool')(x)
     return x
 
-def model(dataset, num_blocks=3, num_layers=12, growth_rate=12, dropout=0.,
-          l2_reg=1e-4, init_channels=16):
+def model(dataset, num_blocks=3, num_layers=12, growth_rate=12,
+          bottleneck=False, compression=1.,
+          dropout=0., l2_reg=1e-4, init_channels=16):
     '''
     Implementation of Densenet[1] model which concatenate all previous layers'
     outputs as the current layer's input.
+
+    If bottleneck is True, each layer is preceded by a 1x1 conv bottleneck
+        (DenseNet-B)
+
+    If compression < 1, each transition block will output
+        output_channels = compression*input_channels (DenseNet-C)
+
+    If both, DenseNet-BC
 
     [1] Huang, Liu and Weinberger. `Densely Connected Convolutional
         Networks`: https://arxiv.org/abs/1608.06993
 
     '''
+    if compression > 1:
+        raise ValueError('Compression rate should be <= one.' +
+                         'Found: compression = %g' %compression)
     num_channels = init_channels
     if dataset in ['cifar10', 'cifar100', 'svhn']:
         x = Input(shape=(32, 32, 3))
@@ -66,21 +102,28 @@ def model(dataset, num_blocks=3, num_layers=12, growth_rate=12, dropout=0.,
     # Initial convolution
     o = Convolution2D(init_channels, 3, 3, border_mode='same',
                       init='he_normal', W_regularizer=l2(l2_reg),
-                      bias=False)(x)
+                      bias=False, name='first_conv')(x)
     for i in range(num_blocks - 1):
         # Create a dense block
-        o = dense_block(o, num_layers, growth_rate, l2_reg, dropout)
+        o = dense_block(o, num_layers, growth_rate, l2_reg, dropout, bottleneck,
+                        name='block%d' %(i + 1))
         # Update the number of channels
         num_channels += num_layers*growth_rate
-        # Transition layer
-        o = transition_block(o, num_channels, l2_reg, dropout)
 
+        num_channels = int(compression * num_channels)
+        # Transition layer
+        o = transition_block(o, num_channels, l2_reg, dropout,
+                             name='transition%d' %(i + 1))
+
+    i += 1
     # Add last dense_block
-    o = dense_block(o, num_layers, growth_rate, l2_reg, dropout)
+    o = dense_block(o, num_layers, growth_rate, l2_reg, dropout, bottleneck,
+                    name='block%d' %(i + 1))
     # Add final BN-Relu
     o = BatchNormalization(gamma_regularizer=l2(l2_reg),
-                             beta_regularizer=l2(l2_reg))(o)
-    o = Activation('relu')(o)
+                           beta_regularizer=l2(l2_reg),
+                           name='last_bn')(o)
+    o = Activation('relu', name='last_relu')(o)
     # Global average pooling
     o = GlobalAveragePooling2D()(o)
     if dataset in ['cifar10', 'svhn']:
